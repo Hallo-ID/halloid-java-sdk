b6aa4c155628b8db97543962247a2e3db7c93ba4
Diego Rojas <darguelles.rojas91@gmail.com>
2023-03-29 22:27:52 +0200

    undo dummy commit

--- src/main/java/com/halloid/sdk/HalloIDJavaSDK.java
+++ src/main/java/com/halloid/sdk/HalloIDJavaSDK.java
@@ -24,7 +24,6 @@ public class HalloIDJavaSDK {
 
     public String generateServiceToken() {
 //        String privateKeyBase64 = Base64.getEncoder().encodeToString(privateKey.getBytes());
-
         String privateKeyBase64 = this.sanitizeKey(privateKey);
         try {
             return tokenService.generateSignedJWT(clientID, privateKeyBase64);
